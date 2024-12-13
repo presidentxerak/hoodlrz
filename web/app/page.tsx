07ca9d037117d8760e38e0974e2ae493057df608
chixx <lucottejeremie@gmail.com>
2024-12-13 19:47:22 +0100

    init foundry

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,7 +1,8 @@
+import Svg from "@/public/xerak0_Create_a_battered_colorful_isometric_minimalistic_illu_6a883a6a-189f-40af-927a-58e9fb1ef0e7_1 2.svg";
 export default function Home() {
   return (
     <div>
-      <h1>Home</h1>
+      <Svg className="w-full h-auto" />
     </div>
   );
 }
